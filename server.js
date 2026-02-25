72023cef2795fc4074421096804d86d00716b570
Shiba-2439 <matthewlozier580@gmail.com>
2026-02-24 19:43:23 -0600

    Begun Renovations: Change API Keys

--- server.js
+++ server.js
@@ -49,14 +49,14 @@ const {
 
 const serviceAccount = {
     type: "service_account",
-    project_id: "matthew-internet-radio",
+    project_id: process.env.PROJECT_ID,
     private_key_id: process.env.SERVICE_ACCOUNT_PRIVATE_KEY_ID,
     private_key: process.env.SERVICE_ACCOUNT_PRIVATE_KEY.replace(/\\NEWLINE/g, '\n'),
     client_email: process.env.CLIENT_EMAIL,
     client_id: process.env.SERVICE_ACCOUNT_CLIENT_ID,
     auth_uri: "https://accounts.google.com/o/oauth2/auth",
     token_uri: "https://oauth2.googleapis.com/token",
-    auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_X509_CERT_URL,
+    auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
     client_x509_cert_url: process.env.CLIENT_X509_CERT_URL,
     universe_domain: "googleapis.com",
 };
